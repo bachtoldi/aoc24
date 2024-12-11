40ac429f1be7e50bd24732880c33d3f0945b85d0
bachtoldi <paschli@gmail.com>
2024-12-12 00:37:30 +0100

    feat(day01): aardvark

---
+++ aoc/Program.cs
@@ -0,0 +1,21 @@
+﻿using aoc.day01;
+
+var foo = new Dictionary<string, Func<Task<string>>> { { "1", Day01.Execute } };
+
+var input = string.Empty;
+
+while (input is not ("q" or "quit"))
+{
+    if (foo.TryGetValue(input, out var value))
+    {
+        Console.WriteLine(await value());
+    }
+    else if (input is not "")
+    {
+        Console.WriteLine("Hayaa what are you doing?");
+        break;
+    }
+
+    Console.WriteLine("get bent");
+    input = Console.ReadLine() ?? string.Empty;
+}
